ea29f3775afbb5f5f5ffb5a03b804a46e09661bf
olim7t <omichallat+github@gmail.com>
2018-02-26 14:30:08 -0800

    [fixup] use passthrough throttler in request harness

--- core/src/test/java/com/datastax/oss/driver/internal/core/cql/RequestHandlerTestHarness.java
+++ core/src/test/java/com/datastax/oss/driver/internal/core/cql/RequestHandlerTestHarness.java
@@ -41,6 +41,7 @@ import com.datastax.oss.driver.internal.core.metadata.LoadBalancingPolicyWrapper
 import com.datastax.oss.driver.internal.core.pool.ChannelPool;
 import com.datastax.oss.driver.internal.core.servererrors.DefaultWriteTypeRegistry;
 import com.datastax.oss.driver.internal.core.session.DefaultSession;
+import com.datastax.oss.driver.internal.core.session.throttling.PassThroughRequestThrottler;
 import com.datastax.oss.driver.internal.core.type.codec.registry.DefaultCodecRegistry;
 import com.datastax.oss.driver.internal.core.util.concurrent.ScheduledTaskCapturingEventLoop;
 import com.datastax.oss.protocol.internal.Frame;
@@ -148,6 +149,8 @@ public class RequestHandlerTestHarness implements AutoCloseable {
         .thenReturn(new DefaultConsistencyLevelRegistry());
 
     Mockito.when(context.writeTypeRegistry()).thenReturn(new DefaultWriteTypeRegistry());
+
+    Mockito.when(context.requestThrottler()).thenReturn(new PassThroughRequestThrottler(context));
   }
 
   public DefaultSession getSession() {
